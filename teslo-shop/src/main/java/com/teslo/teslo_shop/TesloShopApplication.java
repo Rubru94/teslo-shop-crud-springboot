4212e5da6b994b13474eff42e7bdc800e3e7602c
Ruben Rubio <rubenru94@hotmail.com>
2025-01-02 15:36:51 +0100

    feat: Add Spring Initializr base project

---
+++ teslo-shop/src/main/java/com/teslo/teslo_shop/TesloShopApplication.java
@@ -0,0 +1,13 @@
+package com.teslo.teslo_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesloShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesloShopApplication.class, args);
+	}
+
+}
