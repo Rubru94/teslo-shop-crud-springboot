8ee9aadb8545aefeacc255f118494fec53caa1cc
Ruben Rubio <rubenru94@hotmail.com>
2025-01-03 11:20:03 +0100

    feat: Create Product controller

---
+++ teslo-shop/src/main/java/com/teslo/teslo_shop/product/ProductController.java
@@ -0,0 +1,24 @@
+package com.teslo.teslo_shop.product;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping()
+    public List<Product> getProducts() {
+        return this.service.getProducts();
+    }
+
+}
