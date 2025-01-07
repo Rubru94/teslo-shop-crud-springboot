e640ce5891888eb8ddce6bd35c16ef2ae121f4e6
Ruben Rubio <rubenru94@hotmail.com>
2025-01-07 16:03:33 +0100

    feat: Handle NotFound & BadRequest errors

--- teslo-shop/src/main/java/com/teslo/teslo_shop/core/error/ErrorHandler.java
+++ teslo-shop/src/main/java/com/teslo/teslo_shop/core/error/ErrorHandler.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import com.teslo.teslo_shop.core.error.exceptions.BadRequestException;
+import com.teslo.teslo_shop.core.error.exceptions.NotFoundException;
+
 @ControllerAdvice
 public class ErrorHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(Exception ex, WebRequest request) {
+        ErrorDetailRecord errorDetail = new ErrorDetailRecord(ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetail, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> handleBadRequestException(Exception ex, WebRequest request) {
+        ErrorDetailRecord errorDetail = new ErrorDetailRecord(ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
