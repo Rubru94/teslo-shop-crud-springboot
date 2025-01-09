d22a95bcb6a0a7e218956b2224205fb1dd00175a
Ruben Rubio <rubenru94@hotmail.com>
2025-01-09 09:49:17 +0100

    refactor: Remove ErrorHandler boilerplate

--- teslo-shop/src/main/java/com/teslo/teslo_shop/core/error/ErrorHandler.java
+++ teslo-shop/src/main/java/com/teslo/teslo_shop/core/error/ErrorHandler.java
@@ -1,6 +1,7 @@
 package com.teslo.teslo_shop.core.error;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,19 +15,21 @@ public class ErrorHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<?> handleNotFoundException(Exception ex, WebRequest request) {
-        ErrorDetailRecord errorDetail = new ErrorDetailRecord(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetail, HttpStatus.NOT_FOUND);
+        return this.handleException(ex, request, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<?> handleBadRequestException(Exception ex, WebRequest request) {
-        ErrorDetailRecord errorDetail = new ErrorDetailRecord(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+        return this.handleException(ex, request, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
+        return this.handleException(ex, request, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<?> handleException(Exception ex, WebRequest request, HttpStatusCode status) {
         ErrorDetailRecord errorDetail = new ErrorDetailRecord(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetail, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorDetail, status);
     }
 }
