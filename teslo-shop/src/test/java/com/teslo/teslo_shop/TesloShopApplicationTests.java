d9e2ee69bb001f174a81699c08045ea2120d4703
Ruben Rubio <rubenru94@hotmail.com>
2025-02-05 15:45:40 +0100

    test: Configure base tests

--- teslo-shop/src/test/java/com/teslo/teslo_shop/TesloShopApplicationTests.java
+++ teslo-shop/src/test/java/com/teslo/teslo_shop/TesloShopApplicationTests.java
@@ -1,13 +1,29 @@
 package com.teslo.teslo_shop;
 
+import static org.mockito.Mockito.mockStatic;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TesloShopApplicationTests {
 
+	@Mock
+	private SpringApplication springApplication;
+
 	@Test
 	void contextLoads() {
+		// This test ensures that application context is loaded correctly.
+	}
+
+	@Test
+	public void testMain() {
+		try (var mock = mockStatic(SpringApplication.class)) {
+			TesloShopApplication.main(new String[] {});
+			mock.verify(() -> SpringApplication.run(TesloShopApplication.class, new String[] {}));
+		}
 	}
 
 }
